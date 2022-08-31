e201d34c95c0ce70ba0ab155459cb3ac1194e322
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-08-31 17:58:51 -0400

    stroke rect set

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const canvas = document.getElementById('canvas0');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
-CANVAS_WIDTH = canvas.height = 1000;
+CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
@@ -14,24 +14,21 @@ class Enemy {
     this.height = 100;
     this.speed = Math.random() * 4 - 2;
   }
-
   update() {
-    this.x+= this.speed;
-    this.y+= this.speed;
+    this.x += this.speed;
+    this.y += this.speed;
   }
-
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
   }
 };
 
-for (int i = 0; i < numberOfEnemies; i++) { 
-  enemiesArray.push(new Enemy();
+for (let i = 0; i < numberOfEnemies; i++) { 
+  enemiesArray.push(new Enemy());
 }
 
-
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   enemiesArray.forEach(enemy => {
     enemy.update();
     enemy.draw();
