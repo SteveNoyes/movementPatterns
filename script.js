c76a8a42ca5505c03d40c80ec0efe24c0cc980db
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-08-30 21:39:39 -0400

    random placement

--- script.js
+++ script.js
@@ -3,18 +3,31 @@ const canvas = document.getElementById('canvas0');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_WIDTH = canvas.height = 1000;
+const numberOfEnemies = 100;
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+  }
 
-enemy0 = {
-  x: 0,
-  y: 0,
-  width: 200,
-  height: 200,
-}
+  update() {
+    this.x++;
+    this.y++;
+  }
+
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
+
+const enemy0 = new Enemy();
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  enemy0.x++;
-  ctx.fillRect(enemy0.x, enemy0.y, enemy0.width, enemy0.height);
+  enemy0.update();
+  enemy0.draw();
   requestAnimationFrame(animate);
 }
 animate();
